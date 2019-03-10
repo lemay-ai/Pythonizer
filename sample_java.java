0d62f7e0a364cc580c42bfcdaf009114296e4668
Mary Kate MacPherson <mkmacphe@uwaterloo.ca>
2019-03-10 18:11:58 -0400

    Different source

--- sample_java.java
+++ sample_java.java
@@ -1,136 +1,46 @@
-package Solution;
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.example.bigquery;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.TreeSet;
+// [START bigquery_quickstart]
+// Imports the Google Cloud client library
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.Dataset;
+import com.google.cloud.bigquery.DatasetInfo;
 
-public class UnsortedSetTest {
+public class QuickstartSample {
+  public static void main(String... args) throws Exception {
+    // Instantiate a client. If you don't specify credentials when constructing a client, the
+    // client library will look for credentials in the environment, such as the
+    // GOOGLE_APPLICATION_CREDENTIALS environment variable.
+    BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
 
-    public static void main(String[] args) throws Exception {
-        String[] allFileNames = {"hounds.txt", "huckfinn.txt", "oz.txt", "war.txt", "ciaFactBook2008.txt"};
-        String[] noCIA = {"hounds.txt", "huckfinn.txt", "oz.txt", "war.txt"};
-        countWords(new BinarySearchTree<String>(), allFileNames[0]);
-        for(String s : allFileNames) {
-            System.out.println(s);
-            countWordsOurUnsortedSet(s);
-            countWordsOurBinarySearchTree(s);
-            countWordsOurHash(s);
-            countWordsCollection(new TreeSet<String>(), s);
-            int[] result = countWordsCollection(new HashSet<String>(), s);
-            System.out.println(result[0] + " total words.");
-            System.out.println(result[1] + " distinct words.");
-            System.out.println();
-            
-        }
-    }
-    
-    // return total num words, and num distinct words
-    public static int[] countWordsCollection(Collection<String> c, String fileName) throws Exception{
-        c.clear();
-        Scanner fileScanner = new Scanner(new File(fileName));  
-        Stopwatch st = new Stopwatch();
-        st.start();
-        int total = 0;
-        while(fileScanner.hasNext()){
-            c.add(fileScanner.next());
-            total++;
-        }
-        st.stop();
-        System.out.println("Time for " + c.getClass() + " : \n" + st);
-//        System.out.println(c.size() + " distinct words");
-//        System.out.println(total + " total words including duplicates: ");
-        assert total >= c.size();
-        System.out.println();
-        return new int[]{total, c.size()};
-    }
-    
-    
-    // GACKY GACKY GACKY repition. Look into removing repetition with reflection
-    // we assume there will be add and size methods
-    public static int[] countWordsOurHash(String fileName) throws Exception {
-        Scanner fileScanner = new Scanner(new File(fileName));  
-        Stopwatch st = new Stopwatch();
-        UnsortedHashSet<String> c = new UnsortedHashSet<String>();
-        st.start();
-        int total = 0;
-        while(fileScanner.hasNext()) {
-            c.add(fileScanner.next());
-            total++;
-        }
-        st.stop();
-        System.out.println("Time for our hashtable (closed address hashing): \n" + st);
-//        System.out.println(c.size() + " distinct words");
-//        System.out.println(total + " total words including duplicates: ");
-        assert total >= c.size();
-        System.out.println();
-        return new int[]{total, c.size()};
-    }
-    
-    public static int[] countWordsOurUnsortedSet(String fileName) throws Exception {
-        Scanner fileScanner = new Scanner(new File(fileName));  
-        Stopwatch st = new Stopwatch();
-        UnsortedSet<String> c = new UnsortedSet<String>();
-        st.start();
-        int total = 0;
-        while(fileScanner.hasNext()){
-            c.add(fileScanner.next());
-            total++;
-        }
-        st.stop();
-        System.out.println("Time for our unsorted set based on ArrayList: \n" + st);
-//        System.out.println(c.size() + " distinct words");
-//        System.out.println(total + " total words including duplicates: ");
-        assert total >= c.size();
-        System.out.println();
-        return new int[]{total, c.size()};
-    }
-    
-    public static int[] countWordsOurBinarySearchTree(String fileName) throws Exception {
-        Scanner fileScanner = new Scanner(new File(fileName));  
-        Stopwatch st = new Stopwatch();
-        BinarySearchTree<String> c = new BinarySearchTree<String>();
-        st.start();
-        int total = 0;
-        while(fileScanner.hasNext()){
-            c.add(fileScanner.next());
-            total++;
-        }
-        st.stop();
-        System.out.println("Time for our binary search tree: \n" + st);
-//        System.out.println(c.size() + " distinct words");
-//        System.out.println(total + " total words including duplicates: ");
-        assert total >= c.size();
-        System.out.println();
-        return new int[]{total, c.size()};
-    }
-    
-    
-    // a try at reflection. Not working on Binary Search tree from class. 
-    // Hunch. Due to add method taking in Comparable, not Object!
-    // Alterantives: search list of methods for name?
-    public static int[] countWords(Object c, String fileName) throws Exception {
-        Scanner fileScanner = new Scanner(new File(fileName));  
-        Stopwatch st = new Stopwatch();
-        System.out.println(Arrays.toString(c.getClass().getMethods()));
-        Method addMethod = c.getClass().getMethod("add", Object.class);
-        st.start();
-        int total = 0;
-        while(fileScanner.hasNext()){
-            addMethod.invoke(c, fileScanner.next());
-            total++;
-        }
-        st.stop();
-        System.out.println("Time for " + c.getClass() + ": "+ st);
-        Method sizeMethod = c.getClass().getMethod("size");
-        int distictWords = (Integer) sizeMethod.invoke(c);
-//        System.out.println(distictWords + " distinct words");
-//        System.out.println(total + " total words including duplicates: ");
-        System.out.println();
-        return new int[]{total, distictWords};
-    }
+    // The name for the new dataset
+    String datasetName = "my_new_dataset";
+
+    // Prepares a new dataset
+    Dataset dataset = null;
+    DatasetInfo datasetInfo = DatasetInfo.newBuilder(datasetName).build();
+
+    // Creates the dataset
+    dataset = bigquery.create(datasetInfo);
+
+    System.out.printf("Dataset %s created.%n", dataset.getDatasetId().getDataset());
+  }
 }
+// [END bigquery_quickstart]
